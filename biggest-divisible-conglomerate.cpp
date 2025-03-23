6f022564cf9b611738de960593c9c4d280ba5e01
Jacob Port <jport1@fandm.edu>
2025-03-23 13:07:21 -0400

    more debugging

--- biggest-divisible-conglomerate.cpp
+++ biggest-divisible-conglomerate.cpp
@@ -3,6 +3,7 @@
 #include <algorithm> // this includes the sort function
 #include <iostream>
 #include <sstream>
+#include <cassert>
 
 using namespace std;
 
@@ -49,6 +50,7 @@ int index_finder(vector<int> vec, int start){
 
 
 vector<int> bdc_helper(vector<int> input){
+    cout << "input: " << vec_to_string(input) << endl << endl << endl;
 
     //base cases 
     if(input.size() == 1 || input.size() == 0){
@@ -60,16 +62,17 @@ vector<int> bdc_helper(vector<int> input){
 
 
     for(int i = 0; i < input.size(); i++){
-        vector<int> L = {input[i]};
+        vector<int> L = {input[i]}; cout << " (left esablished as " << input[i] << " on i = " << i << endl;
         vector<int> R;
 
-        int j = index_finder(input, i); //index of next div, -1 of NA 
+        int j = index_finder(input, i); //index of next div, -1 of NA (i + 1 is built into the function)
 
         if(j==-1){
             R = {}; 
             
         }
         if(j!= -1){
+            cout << "recursive call (i = " << i << "): " << endl;
             vector<int> Rin(input.begin() + j, input.end()); 
             R = bdc_helper(Rin); 
         }
@@ -77,6 +80,8 @@ vector<int> bdc_helper(vector<int> input){
         vector<int> combined = L; 
         combined.insert(combined.end(), R.begin(), R.end()); 
         candidates.push_back(combined); 
+
+        cout<< i << ":    " << vec_to_string(L) << "<-------->" << vec_to_string(R) << endl;
 
     }
 
@@ -93,10 +98,10 @@ vector<int> bdc_helper(vector<int> input){
 
     // for debugging
 
-    for(int k = 0; k < candidates.size(); k++){
-        cout << vec_to_string(candidates[k]) << endl << endl;
+    // for(int k = 0; k < candidates.size(); k++){
+    //     cout << vec_to_string(candidates[k]) << endl << endl;
 
-    }
+    // }
 
 
     //returns smallest or emtpy lijst if the vector is 1 or 0
@@ -106,6 +111,7 @@ vector<int> bdc_helper(vector<int> input){
     }
     if(currVecSize == 1){
         vector<int> smallest = {input[0]};
+        return smallest;
     }
 
     //otherwise, returns the longest element. 
@@ -117,6 +123,13 @@ vector<int> bdc_helper(vector<int> input){
 
 void test(){
 
+    vector<int> test1 = {}; 
+    assert(biggest_divisible_conglomerate(test1) == test1); 
+
+    vector<int> test2 = {3,11}; 
+    vector<int> result2 = {3}; 
+
+    assert(biggest_divisible_conglomerate(test2) == result2);
 };
 
 int main(){
@@ -124,7 +137,7 @@ int main(){
 
     // vector<int> test = {28, 22, 7, 2, 8, 14, 24, 56};
 
-    vector<int> test = {10, 5, 3, 15, 20};
+    vector<int> test = {9, 10, 5, 3, 15, 20};
     vector<int> BDC = biggest_divisible_conglomerate(test); 
 
 
