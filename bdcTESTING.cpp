935b6ad5eda6eebf4d564f381679a0f279ad3e57
Jacob Port <jport1@fandm.edu>
2025-03-22 21:59:43 -0400

    more changes

--- bdcTESTING.cpp
+++ bdcTESTING.cpp
@@ -3,6 +3,7 @@
 #include <algorithm> // this includes the sort function
 #include <iostream>
 #include <sstream>
+#include <cassert>   
 
 using namespace std;
 
@@ -37,7 +38,7 @@ vector<int> biggest_divisible_conglomerate(vector<int> input){
 
 
 //helper function to return the index of the next division 
-int index_finder(vector<int> vec, int start){
+int index_finder(vector<int> vec, int start){ 
 
     for(int i = start + 1; i < vec.size(); i++){
         if(vec[i] % vec[start] == 0){
@@ -48,12 +49,14 @@ int index_finder(vector<int> vec, int start){
 }
 
 
+
 vector<int> bdc_helper(vector<int> input){
 
     //base cases 
     if(input.size() == 1 || input.size() == 0){
         return input; 
     }
+
 
     //makes a vector of potential candidates: 
     vector<vector<int>> candidates; 
@@ -79,6 +82,44 @@ vector<int> bdc_helper(vector<int> input){
         candidates.push_back(combined); 
 
     }
+
+
+
+//old code: 
+{
+    // //iterates through the differnte starting points of input
+    // for(int i = 0; i < input.size(); i++){
+
+    //     vector<int> L = {input[i]}; //creates left side with just one element. 
+
+    //     int j = index_finder(input,i); //finds the index of the next element that is a multiple of i; returns -1 if no such number exists 
+
+
+    //     if(j != -1){
+    //         //creates a sub vector of input from the above index
+    //         vector<int> Rin(input.begin() + j, input.end()); 
+
+    //         //using recursion to find a vector of all other multiples satifactory elements in input 
+    //         vector<int> R = bdc_helper(Rin); 
+
+    //         //Combines L and R into a vector called combined, then pushes that back to the list of candidates 
+    //         vector<int> combined = L; 
+    //         combined.insert(combined.end(), R.begin(), R.end());
+    //         candidates.push_back(combined); 
+    //         cout << vec_to_string(L) << "      " << vec_to_string(R) << "          " << vec_to_string(combined) << endl;
+    //     }else{
+    //         candidates.push_back(L);
+    //     }
+
+    // }
+
+
+
+
+}
+
+
+
 
     //finds the longest vector in candidates. if no vector in longer than one elemnt, it will return the smallest element of input, using 
     // input[0]
@@ -114,13 +155,23 @@ vector<int> bdc_helper(vector<int> input){
 };
 
 
-
 void test(){
+
+    vector<int> unSorted = {5, 3, 2, 4, 1};
+    sort(unSorted.begin(), unSorted.end()); 
+    vector<int> sorted = {1, 2, 3, 4, 5};
+
+    assert(unSorted == sorted); 
+
+
+
+
 
 };
 
 int main(){
 
+    test();
 
     // vector<int> test = {28, 22, 7, 2, 8, 14, 24, 56};
 
